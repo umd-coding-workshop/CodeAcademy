ce7209c7b2deb919daeb04522abfe5bbeef9192d
Ben Wallberg <wallberg@umd.edu>
2013-09-29 07:40:36 -0400

    add Exercise 1: Lesson Number One

---
+++ python/student-becomes-teacher.py
@@ -0,0 +1,8 @@
+
+# CodeAcademy course: Student Becomes the Teacher
+# http://www.codecademy.com/courses/python-beginner-en-qzsCL
+
+# initialize the student data
+lloyd = {'name':'Lloyd', 'homework':[], 'quizzes':[], 'tests':[]}
+alice = {'name':'Alice', 'homework':[], 'quizzes':[], 'tests':[]}
+tyler = {'name':'Tyler', 'homework':[], 'quizzes':[], 'tests':[]}
